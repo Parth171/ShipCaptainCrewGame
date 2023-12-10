c078666b3b4b0ae98bc417fd57799c99b2d2d29c
Parth Sakpal <p.sakpal@share.espb.ca>
2023-12-10 00:04:10 -0700

    Almost Done game

--- game.py
+++ game.py
@@ -17,99 +17,260 @@ def getPlayers():
 
 
 
-class Multiplayer:
+class Play:
 
-    def __init__(self, PLAYER1:player.Player, PLAYER2:player.Player):
-        self.player1 = PLAYER1
-        self.player2 = PLAYER2
-        self.p1Count = 3
-        self.p2Count = 3
+    def __init__(self, PLAYER:player.Player):
+        self.player = PLAYER
+
+        self.pCount = 3
         self.allFound = False
         self.shipFound = False
         self.captainFound = False
-        #self.crewFound = False
 
     def run(self):
 
-        print(f"{self.player1} roll. Press enter to roll")
-        self.player1.createDices(5)
+        print(f"{self.player} roll. Press ENTER to begin the round!")
+        self.player.createDices(5)
 
-        while self.p1Count > 0:
+        while self.pCount > 0:
 
 
-            input("press enter to continue....")
-            ROLL = self.player1.rollActiveDices()
+            input("Press ENTER to roll you dice.")
+            ROLL = self.player.rollActiveDices()
 
-            print(ROLL)
+            print(f"Your Roll was: {ROLL}")
 
-            self.p1Count = self.p1Count - 1
+            self.pCount = self.pCount - 1
 
             if not self.shipFound:
                 if 6 in ROLL:
-                    print("ship found")
-                    self.player1.createDices(4)
+                    print("\nship found\n")
+                    self.player.createDices(4)
                     self.shipFound = True
 
 
             if self.shipFound == True:
                 if not self.captainFound:
                     if 5 in ROLL:
-                        print("captain found")
-                        self.player1.createDices(3)
+                        print("\ncaptain found\n")
+                        self.player.createDices(3)
                         self.captainFound = True
 
             if self.captainFound == True:
                 if not self.allFound:
                     if 4 in ROLL:
-                        print("crew found")
-                        self.player1.createDices(2)
+                        print("\ncrew found\n")
+                        self.player.createDices(2)
                         self.allFound = True
 
 
 
             if self.allFound == True:
 
+                while self.pCount > 1:
 
-
-                if self.p1Count > 1:
-                    self.player1.createDices(2)
-                    input("press enter to continue....")
-                    GOLD_ROLL = self.player1.rollActiveDices()
+                    self.player.createDices(2)
+                    input("Press ENTER to roll for gold!")
+                    GOLD_ROLL = self.player.rollActiveDices()
                     print(GOLD_ROLL)
 
 
-                    self.player1.playerGold = sum(GOLD_ROLL)
-                    print(self.player1.playerGold)
+                    self.player.playerGold = sum(GOLD_ROLL)
+                    print(self.player.playerGold)
 
-                self.p1Count -= 1
+                    self.pCount -= 1
 
-                INPUT = input("ROLL AGAIN? ")
+                    INPUT = input("ROLL AGAIN? (Y/N): ")
 
-                if INPUT == 'Y':
-                    GOLD_ROLL = self.player1.rollActiveDices()
+                    if INPUT == "Y" or INPUT == "N":
+                        pass
+                    else:
+                        print("Please enter Y or N.")
+                        INPUT = input("ROLL AGAIN? (Y/N): ")
 
+                    if INPUT == 'Y':
+                        GOLD_ROLL = self.player.rollActiveDices()
+
+                        print(f"Your New Roll: {GOLD_ROLL}")
+
+                        self.player.playerGold = sum(GOLD_ROLL)
+                        print(f"you got {self.player.playerGold} pieces of gold!")
+                        return self.player.playerGold
+
+
+                    if INPUT == "N":
+                        if self.player.playerGold == 0:
+                            print("Sorry you got no gold this round :(")
+                            return 0
+                        else:
+                            print(f"you got {self.player.playerGold} pieces of gold!")
+                            return self.player.playerGold
+
+
+
+                if self.pCount == 1:
+                    self.player.createDices(2)
+                    input("Roll for Gold...")
+                    GOLD_ROLL = self.player.rollActiveDices()
+                    print(GOLD_ROLL)
+                    self.player.playerGold = sum(GOLD_ROLL)
+                    if self.player.playerGold == 0:
+                        print("Sorry you got no gold this round :(")
+                        return 0
+                    else:
+                        print(f"you got {self.player.playerGold} pieces of gold!")
+                        return self.player.playerGold
+
+        print("Sorry you got no gold this round :(")
+        return 0
+
+    def computer(self):
+
+        input(f"Computers Roll. Press ENTER to begin the round!")
+        self.player.createDices(5)
+
+        while self.pCount > 0:
+
+            ROLL = self.player.rollActiveDices()
+
+
+            print("Computer Rolling....")
+            print(f"Computer Rolled: {ROLL}")
+
+            self.pCount = self.pCount - 1
+
+            if not self.shipFound:
+                if 6 in ROLL:
+                    print("\nship found\n")
+                    self.player.createDices(4)
+                    self.shipFound = True
+
+
+            if self.shipFound == True:
+                if not self.captainFound:
+                    if 5 in ROLL:
+                        print("\ncaptain found\n")
+                        self.player.createDices(3)
+                        self.captainFound = True
+
+            if self.captainFound == True:
+                if not self.allFound:
+                    if 4 in ROLL:
+                        print("\ncrew found\n")
+                        self.player.createDices(2)
+                        self.allFound = True
+
+
+
+            if self.allFound == True:
+
+                while self.pCount > 1:
+
+                    self.player.createDices(2)
+                    input("Computer Rolling for Gold...")
+                    GOLD_ROLL = self.player.rollActiveDices()
                     print(GOLD_ROLL)
 
-                    self.player1.playerGold = sum(GOLD_ROLL)
-                    print(self.player1.playerGold)
-                    exit()
 
-                elif INPUT == "N":
-                    print(self.player1.playerGold)
-                    exit()
+                    self.player.playerGold = sum(GOLD_ROLL)
+                    print(self.player.playerGold)
 
-        print(self.player1.playerGold)
+                    self.pCount -= 1
 
+                    if self.player.playerGold < 6:
 
+                        print("Computer Rolls Again!")
 
 
+                        GOLD_ROLL = self.player.rollActiveDices()
+
+                        print(f"Computer's New Roll: {GOLD_ROLL}")
+
+                        self.player.playerGold = sum(GOLD_ROLL)
+                        print(f"Computer got {self.player.playerGold} pieces of gold!")
+                        return self.player.playerGold
 
 
+                    else:
+                        if self.player.playerGold == 0:
+                            print("Computer got no gold this round :(")
+                            return 0
+                        else:
+                            print(f"Computer got {self.player.playerGold} pieces of gold!")
+                            return self.player.playerGold
+
+
+
+                if self.pCount == 1:
+                    self.player.createDices(2)
+                    input("Computer Rolling for Gold...")
+                    GOLD_ROLL = self.player.rollActiveDices()
+                    print(GOLD_ROLL)
+                    self.player.playerGold = sum(GOLD_ROLL)
+                    if self.player.playerGold == 0:
+                        print("Computer got no gold this round :(")
+                        return 0
+                    else:
+                        print(f"Computer got {self.player.playerGold} pieces of gold!")
+                        return self.player.playerGold
+
+        print("Computer got no gold this round :(")
+        return 0
+
+    def winner(self, PLAYER1:player.Player, PLAYER2:player.Player):
+
+        if PLAYER1.playerGold > PLAYER2.playerGold:
+            return PLAYER1
+        if PLAYER1.playerGold < PLAYER2.playerGold:
+            return PLAYER2
+        else:
+            print("You Tied! Tie Break!!")
+
+
+
+
+class threeRounds:
+
+    def __init__(self, PLAYER1: player.Player, PLAYER2: player.Player):
+        self.player1 = PLAYER1
+        self.player2 = PLAYER2
+        self.p1Gold = 0
+        self.p2Gold = 0
+
+    def intro(self):
+        print("""
+        ###
+
+        Player one will take three turns
+        Player two will take three turns
+
+        """)
+
+    def run(self):
+
+        for i in range(3):
+            self.p1Gold += Play(self.player1).run()
+
+            self.p2Gold += Play(self.player2).run()
 
 
 
 class SinglePlayer:
-    pass
+
+    def __init__(self, PLAYER:player.Player):
+        self.player = PLAYER
+        self.computer = Player("Computer")
+
+    def run(self):
+
+        Play(self.player).run()
+        Play(self.computer).computer()
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -117,62 +278,9 @@ if __name__ == "__main__":
     P1 = Player("Parth")
     p2 = Player("Moksh")
 
-    GAME = Multiplayer(P1, p2)
+    GAME = SinglePlayer(P1)
     GAME.run()
 
-    """if self.p1Count > 1:
 
-                 self.player1.createDices(2)
-                 input("press enter to continue....")
-                 self.player1.rollActiveDices()
-                 print(self.player1.getRoll())
 
-                 self.player1.playerGold = sum(self.player1.getRoll())
-                 print(self.player1.playerGold)
 
-                 self.p1Count -= 1
-
-                 INPUT = input("ROLL AGAIN? ")
-
-                 if INPUT == 'Y':
-                     input("press enter to continue....")
-                     self.player1.rollActiveDices()
-                     print(self.player1.getRoll())
-
-                     self.player1.playerGold = sum(self.player1.getRoll())
-                     print(self.player1.playerGold)
-
-                 elif INPUT == "N":
-                     return self.player1.playerGold"""
-
-"""if 6 in ROLL and 5 in ROLL and 4 in ROLL:
-           print("All found")
-           self.Found = True
-
-       elif 6 in ROLL and 5 in ROLL:
-           print("Ship and Captain found")
-           self.shipFound = True
-           self.captainFound = True
-
-       elif 6 in ROLL and 5 not in ROLL:
-           print("ship found")
-           self.shipFound = True
-
-       if self.shipFound == True:
-
-           self.player1.createDices(4)
-
-           if 5 in ROLL and 4 in ROLL:  
-               print("captain and crew found")
-               self.captainFound = True
-               self.crewFound = True
-
-           if 5 in ROLL:
-               print("captain found")
-               self.captainFound
-
-       if self.captainFound == True:
-
-           if 4 in ROLL:
-               print("crew found")
-               self.allFound = True"""
